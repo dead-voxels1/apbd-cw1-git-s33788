74d3ef8b518616d4851f633824d53135f58776a7
Jan Wnorowski <s33788@pjwstk.edu.pl>
2026-03-18 23:32:17 +0100

    Added ToDoTask Class. Added task priority and description

--- CW1/CW1/Program.cs
+++ CW1/CW1/Program.cs
@@ -1,22 +1,43 @@
 ﻿
 
+using CW1;
+
 public class MainClass
 {
-    static private List<string> toDo = new List<string>();
+    static private List<ToDoTask> toDo = new List<ToDoTask>();
 
     public static void Main(String[] args)
     {
         
     }
 
-    public static void AddTask(string task)
+    public static void AddTask(ToDoTask task)
     {
-        toDo.Add(task);
+        if (toDo.All(t => t.Name != task.Name))
+        {
+            toDo.Add(task);
+            int index = toDo.Count-1;
+            while (index != 0 && toDo[index].Priority > toDo[index - 1].Priority)
+            {
+                (toDo[index-1], toDo[index]) = (toDo[index], toDo[index-1]);
+                index--;
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Task ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\""+task.Name +"\"");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" already exists");
+            Console.ResetColor();
+        }
     }
 
-    public static void CompleteTask(string task)
+    public static void CompleteTask(ToDoTask task)
     {
-        if (toDo.Contains(task))
+        if (toDo.All(t => t.Name != task.Name))
         {
             toDo.Remove(task);
         }
@@ -25,9 +46,26 @@ public class MainClass
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("Task ");
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.Write("\""+task +"\"");
+            Console.Write("\""+task.Name +"\"");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(" does not exist");
+            Console.ResetColor();
+        }
+    }
+
+    public static void PrintToDoList()
+    {
+        int index = 1;
+        foreach (ToDoTask t in toDo)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(index++ + ". ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(t.Name);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(" " + t.Description);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" " +  t.Priority);
             Console.ResetColor();
         }
     }
---
+++ CW1/CW1/ToDoTask.cs
@@ -0,0 +1,53 @@
+﻿namespace CW1;
+
+public class ToDoTask
+{
+    private String name;
+    private String description;
+    private int priority;
+
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Description
+    {
+        get => description;
+        set => description = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Priority
+    {
+        get => priority;
+        set => priority = value;
+    }
+
+    public ToDoTask(string name, string description, int priority)
+    {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public ToDoTask(string name, int priority)
+    {
+        this.name = name;
+        this.priority = priority;
+        this.description = "";
+    }
+
+    public ToDoTask(string name, string description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    public ToDoTask(string name)
+    {
+        this.name = name;
+        this.priority = 0;
+        this.description = "";
+    }
+}
