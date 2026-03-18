3dafb5d6ffcf0573f2d7d851ef63b4c8f34f791c
Jan Wnorowski <s33788@pjwstk.edu.pl>
2026-03-18 22:49:37 +0100

    Added basic To Do List functionality

--- CW1/CW1/Program.cs
+++ CW1/CW1/Program.cs
@@ -1,9 +1,34 @@
 ﻿
 
-public class Main
+public class MainClass
 {
-    public static void main(String[] args)
+    static private List<string> toDo = new List<string>();
+
+    public static void Main(String[] args)
     {
         
+    }
+
+    public static void AddTask(string task)
+    {
+        toDo.Add(task);
+    }
+
+    public static void CompleteTask(string task)
+    {
+        if (toDo.Contains(task))
+        {
+            toDo.Remove(task);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Task ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\""+task +"\"");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" does not exist");
+            Console.ResetColor();
+        }
     }
 }
